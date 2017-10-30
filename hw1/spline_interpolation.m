f9d45da05f85d8ac1d33901727d493973d161495
New-bottle <775712558@qq.com>
2017-10-30 14:30:57 +0800

    spline-interpolation finished (matrix, solving equtions)

--- hw1/spline_interpolation.m
+++ hw1/spline_interpolation.m
@@ -1,27 +1,58 @@
-function [] = spline_interpolation(n)
+function y0 = spline_interpolation(x, y, f0, fn, x0, kind)
 
-x0 = [-1:2.0/n:1];
-y0 = 1./(1+25.*x0.*x0);
+n = length(x); m = length(x0);
+y0 = zeros(m);
 
-x = [-1:0.01:1];
-n = length(x0); m = length(x);
-y = zeros(m);
-m0 = (-50.*x0)./((1+25.*x0.*x0).^2);
+% 中间变量定义
+miu = zeros(n);
+lambda = zeros(n);
+d = zeros(n);
+h = zeros(n);
 
-for t = 1:m
-	z=x(t);
-	for k=1:n
-		if z<=x0(k+1) 
-			alpha_k = (1+2*(z-x0(k))/(x0(k+1)-x0(k)))*(((z-x0(k+1))/(x0(k)-x0(k+1)))^2);
-			alpha_k1 = (1+2*(z-x0(k+1))/(x0(k)-x0(k+1)))*(((z-x0(k))/(x0(k+1)-x0(k)))^2);
-			beta_k = (z-x0(k))*(((z-x0(k+1))/(x0(k)-x0(k+1)))^2);
-			beta_k1 = (z-x0(k+1))*(((z-x0(k))/(x0(k+1)-x0(k)))^2);
-			y(t) = alpha_k * y0(k) + (alpha_k1 * y0(k+1)) + beta_k * m0(k) + beta_k1 * m0(k+1);
+% 中间变量计算
+for i = 1:n-1
+	h(i) = x(i+1) - x(i);
+end
+for i = 2:n-1
+	miu(i) = h(i-1)/(h(i-1)+h(i));
+	lambda(i) = h(i)/(h(i-1)+h(i));
+	d(i) = 6*( (y(i+1)-y(i))/(x(i+1)-x(i))-(y(i)-y(i-1))/(x(i)-x(i-1)) ) / (h(i-1)+h(i));
+end
+if kind == 1
+	lambda(1) = 1;
+	miu(n) = 1;
+	d(1) = 6/h(1)*((y(2)-y(1))/(x(2)-x(1))-f0);
+	d(n) = 6/h(n-1)*(fn-(y(n-1)-y(n))/(x(n-1)-x(n)));
+end
+if kind == 2
+	lambda(1) = 0;
+	miu(1) = 0;
+	d(1) = 2*f0;
+	d(n) = 2*fn;
+end
+
+% 列出方程
+A = zeros(n,n);
+for i = 1:n
+	A(i,i) = 2;
+	if (i>1)
+		A(i,i-1) = miu(i);
+	end
+	if (i<n)
+		A(i,i+1) = lambda(i);
+	end
+end
+
+M = A\d;
+
+for i = 1:m
+	z = x0(i);
+	for j = 1:(n-1)
+		if z<=x(j+1)
+			y0(i) = M(j)*((x(j+1)-z)^3)/6/h(j)+M(j+1)*((z-x(j))^3)/6/h(j)+(y(j)-M(j)*(h(j)^2)/6)*(x(j+1)-z)/h(j)+...
+				    (y(j+1)-(M(j+1)*h(j)^2)/6)*(z-x(j))/h(j);
 			break;
 		end
 	end
 end
 
-plot(x, y, 'r');
-hold on;
-plot(x, 1./(1+25.*x.*x), 'b');
