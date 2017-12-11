cef7cae63eecf1bb26093b5c8d4679ee3a85a542
New-bottle <775712558@qq.com>
2017-12-11 19:52:41 +0800

    Steffensen ok

--- hw8/Steffensen.m
+++ hw8/Steffensen.m
@@ -1,11 +1,21 @@
-function times = Steffensen(x0, iterator, eps)
+function [xn,times] = Steffensen(x0, iterator, eps)
 
 y0 = iterator(x0);
 z0 = iterator(y0);
-times = 0;
-while (abs(z0 - x0) > eps)
-	x0 = x0 - (y0-z0)^2/(z0-2*y0+x0);
+if (z0-2*y0+x0) == 0
+	xn = x0;
+	times = 0;
+	return;
+end
+xn = x0 - ((y0-x0).^2)/(z0-2*y0+x0);
+times = 1;
+while (abs(xn - x0) > eps)
+	x0 = xn;
 	y0 = iterator(x0);
 	z0 = iterator(y0);
+	if (z0-2*y0+x0) == 0
+		return;
+	end
+	xn = x0 - (y0-x0)^2/(z0-2*y0+x0);
 	times = times + 1;
 end
